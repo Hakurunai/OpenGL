76c1ddfc89ae70f286133a551e49a3fd8e8fd9d8
Hakurunai <167714064+Hakurunai@users.noreply.github.com>
2024-10-15 14:43:58 +0200

    Creating the foundations of our rendering pass and inputs

--- LightBolt/src/LightBolt.cpp
+++ LightBolt/src/LightBolt.cpp
@@ -1,11 +1,18 @@
+#include <glad/glad.h> //glad need to be include before glfw
 #include <GLFW/glfw3.h>
+
+//std
 #include <stdexcept>
 #include <iostream>
 
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+
+void Framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
 int main()
 {
-    GLFWwindow* window;
-
     try
     {
         if (!glfwInit())
@@ -15,16 +22,65 @@ int main()
     {
         std::cout << e.what();
     }
+    //OpenGL 4.6
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    //GLFW 3.4
+    glfwWindowHint(GLFW_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_VERSION_MINOR, 4);
 
-    window = glfwCreateWindow(640, 480, "LightBolt", NULL, NULL);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __Apple__
+    #if TARGET_OS_MAC
+    //if compiled on mac OS x
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif __TARGET_OS_MAC
+#endif __APPLE__
 
-    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+    GLFWwindow* window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "LightBolt", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+    glfwMakeContextCurrent(window);
+
+    //init Glad to load OpenGL functions for us
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return EXIT_FAILURE;
+    }
+    glViewport(0, 0, WIN_WIDTH, WIN_HEIGHT);
+    //if the window is resized, we need to resize the viewport too
+    glfwSetFramebufferSizeCallback(window, Framebuffer_size_callback);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     while (!glfwWindowShouldClose(window))
     {
+        processInput(window);
+
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
         glfwPollEvents();
         glfwSwapBuffers(window);
     }
 
     glfwTerminate();
     return EXIT_SUCCESS;
+}
+
+
+void Framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
--- README.txt
+++ README.txt
@@ -1,5 +1,6 @@
 Using GLFW 3.4 for WIN64, lib-vc2022
 Using Glad
+Using OpenGL 4.6 (not included with LibPreparator)
 
 This application need .NET 8.0. Be sure to get the SDK if necessary following this link :
 To get the necessary libraries, please launch the LibPreparator application at this path :
