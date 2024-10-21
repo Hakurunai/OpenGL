f5ed8779e1482c0eac7822454ed139cff9569e77
Hakurunai <guillaumeal.pro@gmail.com>
2024-10-21 16:07:15 +0200

    Adding a parameter that we can pass from one shader to another

--- LightBolt/src/LightBolt.cpp
+++ LightBolt/src/LightBolt.cpp
@@ -10,9 +10,11 @@
 
 const char* vertexShaderSource = "#version 460 core\n"
 "layout(location = 0) in vec3 aPos;\n"
+"out vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
 "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"    vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
 "}\0";
 
 const char* fragShaderSourceOrange = "#version 460 core\n"
@@ -24,9 +26,10 @@ const char* fragShaderSourceOrange = "#version 460 core\n"
 
 const char* fragShaderSourceYellow = "#version 460 core\n"
 "out vec4 FragColor;\n"
+"in vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
-"    FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+"    FragColor = vertexColor;\n"
 "}\0";
 
 void Framebuffer_size_callback(GLFWwindow* window, int width, int height);
